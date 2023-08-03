ac699c7edc3722aaa6168f60f26952c12733fd69
meleadr <meledina.adriano@gmail.com>
2023-08-03 14:59:33 +0200

    Newsletter subscribe

--- src/app/landing-page/components/home/home.component.ts
+++ src/app/landing-page/components/home/home.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
+  userEmail!: string;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  onSubscribe(emailForm: NgForm) {
+    console.log(emailForm.value);
+  }
 }
--- src/app/landing-page/landing-page.module.ts
+++ src/app/landing-page/landing-page.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import {SharedModule} from "../shared/shared.module";
 import {RouterLink} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 
@@ -14,6 +15,8 @@ import {RouterLink} from "@angular/router";
     CommonModule,
     SharedModule,
     RouterLink,
+    ReactiveFormsModule,
+    FormsModule,
   ]
 })
 export class LandingPageModule { }
