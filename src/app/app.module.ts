a1cec876b0871e8bdc24fd915d00b351b8bd7310
meleadr <meledina.adriano@gmail.com>
2023-08-03 13:18:31 +0200

    Landing page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {CoreModule} from "./core/core.module";
+import {SharedModule} from "./shared/shared.module";
+import {LandingPageModule} from "./landing-page/landing-page.module";
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import {CoreModule} from "./core/core.module";
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
-        CoreModule
+        CoreModule,
+        LandingPageModule,
+        SharedModule
     ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/landing-page/landing-page.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import {SharedModule} from "../shared/shared.module";
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  exports: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    SharedModule,
+  ]
+})
+export class LandingPageModule { }
