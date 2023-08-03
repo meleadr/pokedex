01441485421beb384530aee19d8e219e94fb09b1
meleadr <meledina.adriano@gmail.com>
2023-08-03 14:45:33 +0200

    Auth

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private token!: string;
+
+  getToken() :string{
+    return this.token;
+  }
+
+  login(): void{
+    this.token = 'MyFakeToken';
+  }
+}
