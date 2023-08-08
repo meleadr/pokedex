5816cec7e5a01f1bc710e85475ea492ba192e4f0
meleadr <meledina.adriano@gmail.com>
2023-08-08 16:15:36 +0200

    Update pokemons.module.ts

--- src/app/pokemons/pokemons.module.ts
+++ src/app/pokemons/pokemons.module.ts
@@ -4,22 +4,12 @@ import { CommonModule } from '@angular/common';
 import { PokemonsRoutingModule } from './pokemons-routing.module';
 import { PokemonListComponent } from './components/pokemon-list/pokemon-list.component';
 import { SinglePokemonComponent } from './components/single-pokemon/single-pokemon.component';
-import {PokemonsService} from "./services/pokemons.service";
-import {SharedModule} from "../shared/shared.module";
-
+import { PokemonsService } from './services/pokemons.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    PokemonListComponent,
-    SinglePokemonComponent
-  ],
-  imports: [
-    CommonModule,
-    PokemonsRoutingModule,
-    SharedModule
-  ],
-  providers: [
-    PokemonsService
-  ]
+  declarations: [PokemonListComponent, SinglePokemonComponent],
+  imports: [CommonModule, PokemonsRoutingModule, SharedModule],
+  providers: [PokemonsService],
 })
-export class PokemonsModule { }
+export class PokemonsModule {}
